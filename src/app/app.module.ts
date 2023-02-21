e03c84e4104b2eaf71126c097b518cffdd6b12ff
Lorenzo López Rodríguez <lorenlr14@gmail.com>
2023-02-21 15:19:03 +0100

    Creación del proyecto y los componentes principales

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormComponent } from './components/form/form.component';
+import { UserComponent } from './components/user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FormComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
