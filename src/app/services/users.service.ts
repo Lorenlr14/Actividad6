84fc200dcd11f7c681971f174bfc35255aafcbdf
Lorenzo López Rodríguez <lorenlr14@gmail.com>
2023-02-22 16:59:40 +0100

    Creación de la interfaz y el servicio, además de una bbdd fake.

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { USERS } from '../db/users.db';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  arrUsers: User[] = USERS;
+  constructor() { }
+}
